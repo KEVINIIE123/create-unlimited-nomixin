4c4361c484812d0af9319323a282799a3f7d1a7d
Rhys⁣⁣⁣⁣⁣⁣⁣ de Haan <98863820+rhysdh540@users.noreply.github.com>
2023-02-10 16:22:14 -0500

    remove track placement limit (bad idea)

--- src/main/java/com/simibubi/create/foundation/config/CTrains.java
+++ src/main/java/com/simibubi/create/foundation/config/CTrains.java
@@ -3,7 +3,7 @@ package com.simibubi.create.foundation.config;
 public class CTrains extends ConfigBase {
 
 	public final ConfigBool trainsCauseDamage = b(true, "trainsCauseDamage", Comments.trainsCauseDamage);
-	public final ConfigInt maxTrackPlacementLength = i(32, 16, 128, "maxTrackPlacementLength", Comments.maxTrackPlacementLength);
+	public final ConfigInt maxTrackPlacementLength = i(32, 16, "maxTrackPlacementLength", Comments.maxTrackPlacementLength);
 	public final ConfigInt maxAssemblyLength = i(128, 5, "maxAssemblyLength", Comments.maxAssemblyLength);
 	public final ConfigInt maxBogeyCount = i(20, 1, "maxBogeyCount", Comments.maxBogeyCount);
 	public final ConfigFloat manualTrainSpeedModifier = f(.75f, 0, "manualTrainSpeedModifier", Comments.manualTrainSpeedModifier);
